7a452824b759768badec0b10b2321ea8fdb45f4f
katana <ayoub@qir.ma>
2025-11-06 13:45:47 +0100

    2. Arguments

---
+++ 2-arguments.js
@@ -0,0 +1,14 @@
+const argv = process.argv.slice(2);
+
+if (argv.length <= 0)
+{
+    console.log("No argument");
+}
+else if (argv.length == 1)
+{
+    console.log("Argument found");
+}
+else
+{
+    console.log("Arguments found");
+}
